8bba2d160ef4ae09118dbe95ab8fa103a0c130a4
Tesnim19 <tesnimabdi67@gmail.com>
2023-10-10 11:01:59 +0300

    base for the models

---
+++ db/base.py
@@ -0,0 +1,6 @@
+from db.base_class import Base
+from db.models.user import User
+from db.models.employee import Employee
+from db.models.department import Department
+from db.models.role import Role
+from db.models.employee_history import EmployeeHistory
